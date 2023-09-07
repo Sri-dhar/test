a7b3000b9dde17a1fed6c356e26d47384b2a7611
Sridhar <94095513+Sri-dhar@users.noreply.github.com>
2023-09-07 09:22:59 +0000

    Add files via upload

--- codes_DSA/AlgoLab/LongestCommonSubseq.cpp
+++ codes_DSA/AlgoLab/LongestCommonSubseq.cpp
@@ -29,20 +29,20 @@ int LCSmemo(string a, string b,int m,int n){
 int LCSdp(string a, string b, int m, int n)
 {
     int dp[m+1][n+1];
-        for(int i=0;i<m+1;i++) dp[i][0]=0;
-        for(int i=0;i<n+1;i++) dp[0][i]=0;
+    for(int i=0;i<m+1;i++) dp[i][0]=0;
+    for(int i=0;i<n+1;i++) dp[0][i]=0;
 
-        for(int i=1;i<m+1;i++)
+    for(int i=1;i<m+1;i++)
+    {
+        for(int j=1;j<n+1;j++)
         {
-            for(int j=1;j<n+1;j++)
-            {
-               if(a[i-1]==b[j-1])
-                    dp[i][j]= 1+dp[i-1][j-1];
-               else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
+            if(a[i-1]==b[j-1])
+                dp[i][j]= 1+dp[i-1][j-1];
+            else
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
-        return dp[m][n];
+    }
+    return dp[m][n];
 }
 
 
